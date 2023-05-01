6c62c34e35e7d82580dc28ae59c088bd89e647c7
Raj Lathigra <rajlathigra03@gmail.com>
2023-05-01 16:00:29 +0530

    .

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xE7381B71113C6cA46D440bcd7B22deeb83123598";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
